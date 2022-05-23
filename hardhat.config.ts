888bb68951341342f28219cd38a33c5f8f90ed78
green9016 <marss9016@gmail.com>
2022-05-23 18:42:23 +0800

    chore: add rinkeby chain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,6 +27,11 @@ const config: HardhatUserConfig = {
       url: process.env.ROPSTEN_URL || '',
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
     }
   },
   gasReporter: {
