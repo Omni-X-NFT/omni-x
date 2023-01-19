337345e0c9dfca270aabdca9cb499d2afdc1831b
green9016 <marss9016@gmail.com>
2023-01-20 00:03:53 +0800

    fix: remove nft proxy

--- contracts/transfer/TransferManagerERC721.sol
+++ contracts/transfer/TransferManagerERC721.sol
@@ -3,23 +3,13 @@ pragma solidity ^0.8.0;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {TransferManagerLzBase} from "./TransferManagerLzBase.sol";
-import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "hardhat/console.sol";
 
 /**
  * @title TransferManagerERC721
  * @notice It allows the transfer of ERC721 tokens.
  */
-contract TransferManagerERC721 is TransferManagerLzBase, IERC721Receiver {
-    struct TradingData {
-        address collection;
-        address from;
-        address to;
-        uint tokenId;
-    }
-    mapping (uint => TradingData) _tradingData;
-    uint _nextTradingId;
-
+contract TransferManagerERC721 is TransferManagerLzBase {
     constructor(address _lzEndpoint) 
         TransferManagerLzBase(_lzEndpoint) {
     }
@@ -58,26 +48,7 @@ contract TransferManagerERC721 is TransferManagerLzBase, IERC721Receiver {
         return false;
     }
 
-    function _proxyTransfer(
-        address collection,
-        address from,
-        address to,
-        uint256 tokenId,
-        uint256 
-    ) virtual internal override returns (uint) {
-        ++_nextTradingId;
-
-        _tradingData[_nextTradingId] = TradingData(
-            collection,
-            from,
-            to,
-            tokenId
-        );
-
-        return _nextTradingId;
-    }
-
-    function _directTransfer(
+    function _normalTransfer(
         address collection,
         address from,
         address to,
@@ -85,25 +56,5 @@ contract TransferManagerERC721 is TransferManagerLzBase, IERC721Receiver {
         uint256 
     ) virtual internal override {
         IERC721(collection).safeTransferFrom(from, to, tokenId);
-    }
-
-    function _shipTransfer(uint tradingId) virtual internal override {
-        IERC721(_tradingData[tradingId].collection).safeTransferFrom(
-            address(this),
-            _tradingData[tradingId].to,
-            _tradingData[tradingId].tokenId
-        );
-
-        delete _tradingData[tradingId];
-    }
-
-    function _revertTransfer(uint tradingId) virtual internal override {
-        IERC721(_tradingData[tradingId].collection).safeTransferFrom(
-            address(this),
-            _tradingData[tradingId].from,
-            _tradingData[tradingId].tokenId
-        );
-
-        delete _tradingData[tradingId];
     }
 }
