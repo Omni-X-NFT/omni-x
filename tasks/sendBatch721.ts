8c0e2cce08cd11054845bdde771ef984f49cce9e
Alejandro Gala <alejandrogala@Alejandros-MacBook-Pro-2.local>
2023-05-17 13:41:29 -0400

    fix metis

--- tasks/sendBatch721.ts
+++ tasks/sendBatch721.ts
@@ -13,20 +13,24 @@ const tx = async (tx1: any) => {
 export const sendBatch721 = async function (taskArgs: any, hre: any) {
   const { ethers, network } = hre
   const [owner] = await ethers.getSigners()
-  const dstChainId = 10132
+  const dstChainId = 101
 
 
   const advancedONFT721Gasless = createContractByName(hre, 'AdvancedONFT721Gasless', AdvancedONFT721GaslessAbi().abi, owner)
 
-  const gas = await advancedONFT721Gasless.estimateSendBatchFee(dstChainId, owner.address, [8504,8505, 8506, 8507], false, ethers.utils.solidityPack(['uint16', 'uint256'], [1, 200000 + 120000]))
+  // const gas = await advancedONFT721Gasless.estimateSendBatchFee(dstChainId, owner.address, [8504,8505, 8506, 8507], false, ethers.utils.solidityPack(['uint16', 'uint256'], [1, 200000 + 120000]))
+  // console.log(gas)
+  // await tx(await advancedONFT721Gasless.sendBatchFrom(
+  //   owner.address,
+  //   dstChainId,
+  //   owner.address,
+  //   [8504,8505, 8506, 8507],
+  //   owner.address,
+  //   ethers.constants.AddressZero,
+  //   ethers.utils.solidityPack(['uint16', 'uint256'], [1,200000 + 120000 ]), {value: gas.nativeFee.toString()}
+  //   ))
+
+  const gas = await advancedONFT721Gasless.estimateSendFee(dstChainId, owner.address, 4, false, ethers.utils.solidityPack(['uint16', 'uint256'], [1, 200000 + 120000]))
   console.log(gas)
-  await tx(await advancedONFT721Gasless.sendBatchFrom(
-    owner.address,
-    dstChainId,
-    owner.address,
-    [8504,8505, 8506, 8507],
-    owner.address,
-    ethers.constants.AddressZero,
-    ethers.utils.solidityPack(['uint16', 'uint256'], [1,200000 + 120000 ]), {value: gas.nativeFee.toString()}
-    ))
+
 }
